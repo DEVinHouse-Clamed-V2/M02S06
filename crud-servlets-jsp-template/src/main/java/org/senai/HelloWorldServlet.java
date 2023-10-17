42bdce39ab4ba296d5c169bc99b295003b442ff1
Ronyeri Marinho <76921589+ronny-souza@users.noreply.github.com>
2023-10-16 21:19:28 -0300

    Template base para o CRUD com JSP

---
+++ crud-servlets-jsp-template/src/main/java/org/senai/HelloWorldServlet.java
@@ -0,0 +1,23 @@
+package org.senai;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello-world")
+public class HelloWorldServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter outputWriter = response.getWriter();
+		outputWriter.println("<html><body><h1>Hello World com Servlets!</h1></body></html>");
+	}
+}
